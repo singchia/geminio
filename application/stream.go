6d6f7229b8d95b6503dc572258e7560fe68ca345
singchia <singchia@163.com>
2023-07-28 23:40:04 +0800

    feat: define request and response and rpc

--- application/stream.go
+++ application/stream.go
@@ -2,15 +2,16 @@ package application
 
 import (
 	"log"
-	"sync"
 
+	"github.com/jumboframes/armorigo/synchub"
+	"github.com/singchia/geminio"
 	"github.com/singchia/geminio/conn"
 	"github.com/singchia/geminio/multiplexer"
 	"github.com/singchia/geminio/packet"
 	"github.com/singchia/go-timer/v2"
 )
 
-type sessionOpts struct {
+type streamOpts struct {
 	// packet factory
 	pf *packet.PacketFactory
 	// logger
@@ -22,11 +23,20 @@ type sessionOpts struct {
 	meta []byte
 }
 
-type session struct {
+type stream struct {
+	// options
+	streamOpts
+
+	// misc
+	shub *synchub.SyncHub
+
 	// under layer dialogue and connection
 	dg multiplexer.Dialogue
 	cn conn.Conn
 
 	// registered rpcs
-	localRPCs sync.Map // key: method
+	localRPCs map[string]geminio.RPC // key: method value: RPC
+
+	// hijacks
+	hijackRPC geminio.HijackRPC
 }
--- geminio.go
+++ geminio.go
@@ -1,1 +1,37 @@
 package geminio
+
+import "time"
+
+type Request interface {
+	// those meta info shouldn't be changed
+	RequestID() uint64
+	StreamID() uint64
+	ClientID() uint64
+	Method() string
+	TImeout() time.Duration
+
+	// application data
+	Data() []byte
+	SetData([]byte)
+}
+
+type Response interface {
+	// those meta info shouldn't be changed
+	RequestID() uint64
+	StreamID() uint64
+	ClientID() uint64
+	Method() string
+	TImeout() time.Duration
+
+	// application data
+	Data() []byte
+	SetData([]byte)
+	Error() error
+	SetError(error)
+}
+
+// rpc functions
+type RPC func(Request, Response)
+
+// hijack rpc functions
+type HijackRPC func(string, Request, Response)
