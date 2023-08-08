e40efb088fd28f9eab35ee89a415504e255c5af7
singchia <singchia@163.com>
2023-08-08 23:40:40 +0800

    add types and response, register packet handler

---
+++ application/types.go
@@ -0,0 +1,88 @@
+package application
+
+import "time"
+
+// request implements geminio.Request
+type request struct {
+	data     []byte
+	id       uint64
+	method   string
+	clientID uint64
+	streamID uint64
+	timeout  time.Duration
+}
+
+// Get RequestID, which is packetID at under layer
+func (req *request) RequestID() uint64 {
+	return req.id
+}
+
+// Get StreamID for the request
+func (req *request) StreamID() uint64 {
+	return req.streamID
+}
+
+// Get ClientID for the request
+func (req *request) ClientID() uint64 {
+	return req.clientID
+}
+
+// Get Method for the request
+func (req *request) Method() string {
+	return req.method
+}
+
+// Get Timeout for the request, if no timeout set, the return.IsZero() is true
+func (req *request) Timeout() time.Duration {
+	return req.timeout
+}
+
+// Get Data for the request
+func (req *request) Data() []byte {
+	return req.data
+}
+
+// response implements geminio.Response
+type response struct {
+	err    error
+	data   []byte
+	method string
+	custom []byte
+	// response share id with requestID, distinguish by packet type
+	requestID uint64
+	clientID  uint64
+	streamID  uint64
+}
+
+func (rsp *response) Error() error {
+	return rsp.err
+}
+
+func (rsp *response) SetError(err error) {
+	rsp.err = err
+}
+
+func (rsp *response) RequestID() uint64 {
+	return rsp.requestID
+}
+
+func (rsp *response) StreamID() uint64 {
+	return rsp.streamID
+}
+
+func (rsp *response) ClientID() uint64 {
+	return rsp.clientID
+}
+
+func (rsp *response) Method() string {
+	return rsp.method
+}
+
+func (rsp *response) Data() []byte {
+	return rsp.data
+}
+
+// Set Data to response, set it in RPC.
+func (rsp *response) SetData(data []byte) {
+	rsp.data = data
+}
