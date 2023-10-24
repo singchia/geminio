c53f7e56cd504d190bbbb2dd8d5701f5ac24973c
singchia <singchia@163.com>
2023-10-24 18:45:20 +0800

    optimized mq

--- examples/mq/broker/main.go
+++ examples/mq/broker/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/jumboframes/armorigo/log"
 	"github.com/jumboframes/armorigo/sigaction"
 	"github.com/singchia/geminio/server"
+	"github.com/singchia/go-timer/v2"
 )
 
 func main() {
@@ -37,9 +38,12 @@ func main() {
 	// log for geminio
 	glog := log.NewLog()
 	glog.SetLevel(lvl)
+	// timer
+	tmr := timer.NewTimer()
 	// accept ends
 	opt := server.NewEndOptions()
 	opt.SetLog(glog)
+	opt.SetTimer(tmr)
 	ln, err := server.Listen("tcp", *broker, opt)
 	if err != nil {
 		log.Errorf("server listen err: %s", err)
